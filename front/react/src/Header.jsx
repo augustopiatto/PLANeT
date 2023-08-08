785e4a40809b91d5014e0e604b23363798cd8982
Augusto <ampiatto@hotmail.com>
2023-08-08 12:52:24 -0300

    adiciona icones do font awesome

--- front/react/src/Header.jsx
+++ front/react/src/Header.jsx
@@ -1,8 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function Header() {
   return (
     <div className="h-[48px] bg-slate-100 flex">
       <h2>PLANeT</h2>
-      <div>icone</div>
+      <FontAwesomeIcon icon={["fas", "fa-star"]} />
     </div>
   );
 }
---
+++ front/react/src/assets/icons/fontawesome.js
@@ -0,0 +1,7 @@
+// Tutorialzim pra caso precise
+// https://www.digitalocean.com/community/tutorials/how-to-use-font-awesome-5-with-react
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faStar);
