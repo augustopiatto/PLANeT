bcea29cc9ce60b2d7beb7af812b08854d6dc924c
Augusto <ampiatto@hotmail.com>
2023-08-08 13:54:22 -0300

    footer

--- front/react/src/Footer.jsx
+++ front/react/src/Footer.jsx
@@ -1,8 +1,13 @@
 function Footer() {
   return (
-    <>
-      <p>Footer</p>
-    </>
+    <div className="h-[64px] py-[12px] bg-slate-50 border-t shadow-[0_-4px_6px_-1px_rgb(0,0,0,0.1),_0_-2px_4px_-2px_rgb(0,0,0,0.1)]">
+      <p className="text-sm text-slate-400 text-center">
+        Cuide bem de suas plantas, que elas irão cuidar bem de você.
+      </p>
+      <p className="text-sm text-slate-400 text-center">
+        Todos direitos reservados
+      </p>
+    </div>
   );
 }
 
--- front/react/src/Header.jsx
+++ front/react/src/Header.jsx
@@ -2,13 +2,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
   return (
-    <div className="h-[48px] bg-slate-100 grid grid-cols-10 border-b shadow-md content-center">
+    <div className="h-[64px] bg-slate-100 grid grid-cols-10 content-center border-b shadow-md">
       <FontAwesomeIcon
         icon={["fas", "fa-angles-right"]}
-        className="text-2xl px-[16px]"
+        className="text-2xl px-[16px] cursor-pointer"
       />
       <h1 className="col-span-8">PLANeT</h1>
-      <FontAwesomeIcon icon={["fas", "fa-star"]} className="text-2xl" />
+      <FontAwesomeIcon
+        icon={["fas", "fa-star"]}
+        className="text-2xl cursor-pointer"
+      />
     </div>
   );
 }
