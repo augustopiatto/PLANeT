09bcfec0e0f35287ad02aa9bea11dbbad7993626
Augusto <ampiatto@hotmail.com>
2023-08-08 13:33:44 -0300

    header

--- front/react/src/Header.jsx
+++ front/react/src/Header.jsx
@@ -2,9 +2,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
   return (
-    <div className="h-[48px] bg-slate-100 flex">
-      <h2>PLANeT</h2>
-      <FontAwesomeIcon icon={["fas", "fa-star"]} />
+    <div className="h-[48px] bg-slate-100 grid grid-cols-10 border-b shadow-md content-center">
+      <FontAwesomeIcon
+        icon={["fas", "fa-angles-right"]}
+        className="text-2xl px-[16px]"
+      />
+      <h1 className="col-span-8">PLANeT</h1>
+      <FontAwesomeIcon icon={["fas", "fa-star"]} className="text-2xl" />
     </div>
   );
 }
--- front/react/src/assets/icons/fontawesome.js
+++ front/react/src/assets/icons/fontawesome.js
@@ -2,6 +2,8 @@
 // https://www.digitalocean.com/community/tutorials/how-to-use-font-awesome-5-with-react
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faStar);
+// solid
+import { faStar, faAnglesRight } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faStar, faAnglesRight);
