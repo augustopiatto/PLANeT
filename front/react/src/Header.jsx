ee255c426f0408d533c72f29e7fe554dec156a31
Augusto <ampiatto@hotmail.com>
2023-08-10 07:35:51 -0300

    Muda um pouco o css do botao

--- front/react/src/Header.jsx
+++ front/react/src/Header.jsx
@@ -5,13 +5,14 @@ const Header = () => {
   return (
     // TODO: ver jeito melhor de montar essas classes
     <div className="h-[64px] bg-clear-dirt grid grid-cols-10 content-center shadow-dirt-md relative">
-      <div className="h-[40px] grid content-center justify-items-center">
+      {/* Ainda não sei se vai ter */}
+      {/* <div className="h-[40px] grid content-center justify-items-center">
         <FontAwesomeIcon
           icon={["fas", "fa-angles-right"]}
-          className="text-2xl px-[16px] cursor-pointer"
+          className="text-2xl cursor-pointer text-dirtier fill-current"
         />
-      </div>
-      <div className="col-span-7 h-[40px] grid content-center justify-items-start">
+      </div> */}
+      <div className="col-span-8 h-[40px] px-[24px] grid content-center justify-items-start">
         <h1>PLANeT</h1>
       </div>
       <PlanetSvg />
@@ -21,7 +22,7 @@ const Header = () => {
           className="text-2xl cursor-pointer text-amber-300 fill-current"
         />
       </div>
-      <div className="h-[40px] w-[40px] rounded-full bg-slate-300 grid content-center justify-items-center">
+      <div className="h-[40px] w-[40px] rounded-full border-2 border-clear-dirt bg-slate-300 grid content-center justify-items-center">
         <FontAwesomeIcon
           icon={["fas", "fa-user-tie"]}
           className="text-2xl cursor-pointer text-slate-500 fill-current"
--- front/react/tailwind.config.js
+++ front/react/tailwind.config.js
@@ -7,19 +7,21 @@ export default {
         "dirt-md":
           "0 4px 6px 2px rgb(84, 55, 11, 0.3), 0 2px 4px 4px rgb(84, 55, 11, 0.3)",
         "dirt-md-up": "0 -4px 6px 2px rgb(84, 55, 11, 0.3)",
-        emphasis: "0 0 4px 0 rgb(88,181,56,0.8)",
+        emphasis:
+          "0 4px 5px 3px rgb(84,55,11,0.8), 0 -2px 5px 2px rgb(88,181,56,0.8)",
       },
       backgroundImage: {
         // verde: #58B538
         // marrom: #54370B
         terrario:
-          "linear-gradient(177deg, rgba(88,181,56,0.97) 0%, rgba(84,55,11,0.95) 95%)",
+          "linear-gradient(180deg, rgba(120,160,90,0.8) 0%, rgba(84,55,11,0.65) 35%)",
         "vine-background":
           "linear-gradient(rgba(255,255,255,0.98), rgba(255,255,255,0.98)), url(./src/assets/images/leaves.jpg)",
-        // linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url("https://i.imgur.com/xnh5x47.jpg");
       },
       colors: {
         plant: "rgba(88, 181, 56, 0.5)",
+        dirtier: "rgba(84, 55, 11, 0.8)",
+        dirt: "rgba(84, 55, 11, 0.6)",
         "clear-dirt": "rgba(84, 55, 11, 0.4)",
         "clearer-dirt": "rgba(84, 55, 11, 0.2)",
       },
